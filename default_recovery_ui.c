5a5330914fc963e4f5c2c6b68ba9822202e6428b
j_r0dd <jree2kx@gmail.com>
2011-05-17 13:04:34 -0400

    proper use of capacitive buttons w/ toggle menu

--- default_recovery_ui.c
+++ default_recovery_ui.c
@@ -41,8 +41,7 @@ int device_toggle_display(volatile char* key_pressed, int key_code) {
         return 1;
     // allow toggling of the display if the correct key is pressed, and the display toggle is allowed or the display is currently off
     if (ui_get_showing_back_button()) {
-        return 0;
-        //return get_allow_toggle_display() && (key_code == KEY_HOME || key_code == KEY_MENU || key_code == KEY_END);
+        return get_allow_toggle_display() && (key_code == KEY_HOME || key_code == KEY_MENU || key_code == KEY_END);
     }
     return get_allow_toggle_display() && (key_code == KEY_HOME || key_code == KEY_MENU || key_code == KEY_POWER || key_code == KEY_END);
 }
@@ -57,13 +56,11 @@ int device_handle_key(int key_code, int visible) {
             case KEY_CAPSLOCK:
             case KEY_DOWN:
             case KEY_VOLUMEDOWN:
-            case KEY_MENU:
                 return HIGHLIGHT_DOWN;
 
             case KEY_LEFTSHIFT:
             case KEY_UP:
             case KEY_VOLUMEUP:
-            case KEY_HOME:
                 return HIGHLIGHT_UP;
 
             case KEY_POWER:
@@ -84,14 +81,9 @@ int device_handle_key(int key_code, int visible) {
             
             case KEY_END:
             case KEY_BACKSPACE:
-            case KEY_SEARCH:
-                if (ui_get_showing_back_button()) {
-                    return SELECT_ITEM;
-                }
+            case KEY_BACK:
                 if (!get_allow_toggle_display())
                     return GO_BACK;
-            case KEY_BACK:
-                return GO_BACK;
         }
     }
 
