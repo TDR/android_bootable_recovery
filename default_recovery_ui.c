1a14f8d42c329f3061d5d6f9218d5300d742df56
Takazumi Matsumoto <takazumi@gmail.com>
2011-10-11 17:42:55 +0000

    Custom key config

--- default_recovery_ui.c
+++ default_recovery_ui.c
@@ -44,7 +44,7 @@ int device_toggle_display(volatile char* key_pressed, int key_code) {
         return 0;
         //return get_allow_toggle_display() && (key_code == KEY_HOME || key_code == KEY_MENU || key_code == KEY_END);
     }
-    return get_allow_toggle_display() && (key_code == KEY_HOME || key_code == KEY_MENU || key_code == KEY_POWER || key_code == KEY_END);
+    return get_allow_toggle_display() && (key_code == KEY_MENU || key_code == KEY_BACK);
 }
 
 int device_reboot_now(volatile char* key_pressed, int key_code) {
@@ -57,14 +57,16 @@ int device_handle_key(int key_code, int visible) {
             case KEY_CAPSLOCK:
             case KEY_DOWN:
             case KEY_VOLUMEDOWN:
-            case KEY_MENU:
                 return HIGHLIGHT_DOWN;
 
+            case KEY_MENU:
             case KEY_LEFTSHIFT:
             case KEY_UP:
             case KEY_VOLUMEUP:
-            case KEY_HOME:
                 return HIGHLIGHT_UP;
+
+            case KEY_HOME:
+                return SELECT_ITEM;
 
             case KEY_POWER:
                 if (ui_get_showing_back_button()) {
@@ -79,19 +81,12 @@ int device_handle_key(int key_code, int visible) {
             case KEY_CENTER:
             case KEY_CAMERA:
             case KEY_F21:
-            case KEY_SEND:
-                return SELECT_ITEM;
-            
+            case KEY_SEND:           
             case KEY_END:
             case KEY_BACKSPACE:
             case KEY_SEARCH:
-                if (ui_get_showing_back_button()) {
-                    return SELECT_ITEM;
-                }
-                if (!get_allow_toggle_display())
-                    return GO_BACK;
             case KEY_BACK:
-                return GO_BACK;
+                return NO_ACTION;
         }
     }
 
